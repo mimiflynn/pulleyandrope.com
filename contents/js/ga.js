886fd9e3adb60cb751bd66610d143248074a9fea
Mimi Flynn <mimiflynn@gmail.com>
2014-02-27 08:36:40 -0500

    ga code update

--- contents/js/ga.js
+++ contents/js/ga.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-27158620-2', 'pulleyandrope.com');
+  ga('create', 'UA-40115848-1', 'pulleyandrope.com');
   ga('send', 'pageview');
